a6aebb4b0a515a652c4153d1f2648fda367619d9
hbk671104 <hbk671104@gmail.com>
2015-10-04 10:07:49 +0800

    Initial Commit

---
+++ ComputerAvailCheck/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ComputerAvailCheck
+//
+//  Created by BK on 10/4/15.
+//  Copyright © 2015 Bokang Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
