9bc1290e7aacadcaed3a9981a4d5af110850cd54
hbk671104 <hbk671104@gmail.com>
2015-10-04 11:03:27 +0800

    let the swift begin

--- ComputerAvailCheck/ViewController.swift
+++ ComputerAvailCheck/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
+		
+		let soap = SOAPEngine()
+		soap.userAgent = "SOAPEngine"
 	}
 
 	override func didReceiveMemoryWarning() {
---
+++ Podfile
@@ -0,0 +1,9 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '8.0'
+
+pod 'SOAPEngine', '~> 1.24'
+
+target 'ComputerAvailCheck' do
+
+end
+
