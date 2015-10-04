59dfb7516d28e703586d2aa4fe97031aeaf0d135
hbk671104 <hbk671104@gmail.com>
2015-10-04 11:30:58 +0800

    file structure changed

--- ComputerAvailCheck/ViewController.swift
+++ ComputerAvailCheck/ViewController.swift
@@ -13,9 +13,6 @@ class ViewController: UIViewController {
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
-		
-		let soap = SOAPEngine()
-		soap.userAgent = "SOAPEngine"
 	}
 
 	override func didReceiveMemoryWarning() {
