566809f5e8ff18d55d3c4ab435ff6c5343edbb0b
hbk671104 <hbk671104@gmail.com>
2016-01-02 23:52:13 +0800

    add apple watch extension

---
+++ WatchCheck Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  WatchCheck Extension
+//
+//  Created by BK on 1/2/16.
+//  Copyright © 2016 Bokang Huang. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
