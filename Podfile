035a1c501dc0a976521f12e2ddc195a16fb5f015
hbk671104 <hbk671104@gmail.com>
2016-01-09 20:01:28 +0800

    completes main interface

--- Podfile
+++ Podfile
@@ -2,12 +2,16 @@
 # platform :ios, '9.0'
 use_frameworks!
 
+def shared_pods
+	pod 'EVReflection', '~> 2.9'
+end
+
 target 'ComputerAvailCheck' do
     platform :ios, '9.0'
     pod 'SOAPEngine', '~> 1.24'
-    pod 'EVReflection', '~> 2.9'
     pod 'AMScrollingNavbar', '~> 2.0'
     pod 'Whisper', '~> 1.0'
+	shared_pods
 end
 
 target 'WatchCheck' do
@@ -16,5 +20,5 @@ end
 
 target 'WatchCheck Extension' do
     platform :watchos, '2.0'
-    pod 'EVReflection', '~> 2.9'
+    shared_pods
 end
--- WatchCheck Extension/InterfaceController.swift
+++ WatchCheck Extension/InterfaceController.swift
@@ -16,10 +16,12 @@ class InterfaceController: WKInterfaceController {
     @IBOutlet var windowLabel: WKInterfaceLabel!
     @IBOutlet var macLabel: WKInterfaceLabel!
     @IBOutlet var linuxLabel: WKInterfaceLabel!
+	
+	var buildingModels:[BuildingModel] = []
     var connectivitySession:WCSession? {
         didSet {
             if let session = connectivitySession {
-                session.delegate = self
+				session.delegate = self
                 session.activateSession()
             }
         }
@@ -30,16 +32,50 @@ class InterfaceController: WKInterfaceController {
         
         // Configure interface objects here.
         connectivitySession = WCSession.defaultSession()
-        // Request latest data
-        connectivitySession?.sendMessage(["data": "haha"], replyHandler: { (response) -> Void in
-            if let data = response["data"] as? NSArray {
-                print(data)
-            }
-            }, errorHandler: { (error) -> Void in
-                print(error)
-        })
+		// "Hide" the labels
+		self.totalLabel.setAlpha(0)
+		self.windowLabel.setAlpha(0)
+		self.macLabel.setAlpha(0)
+		self.linuxLabel.setAlpha(0)
     }
 
+	override func didAppear() {
+		super.didAppear()
+		
+		var totalAvails = 0
+		var totalWin = 0
+		var totalMac = 0
+		var totalLinux = 0
+		// Request latest data from ios
+		connectivitySession?.sendMessage(["data": "haha"], replyHandler: { (response) -> Void in
+			if let data = response["data"] as? NSArray {
+				// Map to models
+				for dict in data {
+					let buildingModel = BuildingModel(dictionary: dict as! NSDictionary)
+					// total avails
+					totalAvails += buildingModel.nAvailable
+					totalWin += buildingModel.nWindows
+					totalMac += buildingModel.nMacintosh
+					totalLinux += buildingModel.nLinux
+					self.buildingModels.append(buildingModel)
+				}
+				// update labels
+				self.totalLabel.setText("\(totalAvails)")
+				self.windowLabel.setText("\(totalWin)")
+				self.macLabel.setText("\(totalMac)")
+				self.linuxLabel.setText("\(totalLinux)")
+				self.animateWithDuration(1.0, animations: { () -> Void in
+					self.totalLabel.setAlpha(1)
+					self.windowLabel.setAlpha(1)
+					self.macLabel.setAlpha(1)
+					self.linuxLabel.setAlpha(1)
+				})
+			}
+			}, errorHandler: { (error) -> Void in
+				print(error)
+		})
+	}
+	
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
@@ -56,7 +92,7 @@ extension InterfaceController: WCSessionDelegate {
     
     // MARK: WCSessionDelegate
     internal func session(session: WCSession, didReceiveMessage message: [String : AnyObject]) {
-        
+        print(message["data"])
     }
     
 }
